29e711fdfdf2161fbe1b6d7e85490d868faa89ef
reza <reza.abraham@uksw.edu>
2025-03-17 10:51:41 +0700

    add button edit dan hapus

--- app/Views/brands/list.php
+++ app/Views/brands/list.php
@@ -61,24 +61,9 @@
                                 <span class="fw-bold"><?= $brand['brand_code'] ?></span>
                             </td>
 
-
                             <td class="text-end">
-                                <a href="#" class="btn btn-sm btn-light btn-flex btn-center btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
-                                    <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
-                                <!--begin::Menu-->
-                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <a href="apps/ecommerce/catalog/edit-product.html" class="menu-link px-3" onclick="alert('Belum Tersedia');return false;">Edit</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <a href="#" class="menu-link px-3" data-kt-ecommerce-product-filter="delete_row" onclick="alert('Belum Tersedia');return false;">Delete</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                </div>
-                                <!--end::Menu-->
+                                <button class="btn btn-sm btn-light-primary me-5" onclick="alert('Belum Tersedia');return false;">Edit</button>
+                                <button class="btn btn-sm btn-light-danger" onclick="alert('Belum Tersedia');return false;">Hapus</button>
                             </td>
                         </tr>
                     <?php endforeach ?>
